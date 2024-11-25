8f8e193bd30a7b38d2f1c9962f96f0663a938a0e
shilajitgr <shshkush@gmail.com>
2024-11-25 16:33:08 +0530

    identity details field added

--- function_app.py
+++ function_app.py
@@ -76,6 +76,7 @@ def requeue_trigger(azqueue: func.QueueMessage):
         logging.info(f"Network interface {data.get('nic_name')} created")
     
     if not entity.get("IdentityCreated", False):
+        entity["IdentityDetails"] = f"{[base_resource_group_name, "DriverHostVMAccess", "eastus"]}"
         identity = provisioner.create_user_assigned_identity(base_resource_group_name, "DriverHostVMAccess", "eastus")
         entity["IdentityCreated"] = identity.id
         azure_table.update_entity(entity)
